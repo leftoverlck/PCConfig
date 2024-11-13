85d5ea6cbf36d4464b244c5260f0f3e657d59283
yuliacheketa <cheketa.yuliia@student.uzhnu.edu.ua>
2024-11-13 06:13:51 +0200

    lab9

---
+++ cypress/support/e2e.js
@@ -0,0 +1,14 @@
+// cypress/support/e2e.js
+
+// Example of setting up custom commands or configurations
+Cypress.Commands.add('login', (username, password) => {
+    cy.get('input[name="username"]').type(username);
+    cy.get('input[name="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+  });
+  
+  // Setup global configuration
+  beforeEach(() => {
+    cy.visit('http://localhost:3000'); // Відкриває стартову сторінку перед кожним тестом
+  });
+  
--- src/ffirebase/deleteConfiguration.js
+++ src/ffirebase/deleteConfiguration.js
@@ -1,17 +1,16 @@
-// src/utils/functions.js
-import { deleteDoc, doc } from 'firebase/firestore';
 
-import { db as database } from '../firebaseConfig';
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
-// Функція для видалення конфігурації з Firestore
+
 export const deleteConfiguration = async (user, configId, setConfigurations, configurations) => {
-    try {
-        const configReference = doc(database, 'user_configurations', user.uid, 'configurations', configId);
-        await deleteDoc(configReference);
-        setConfigurations(configurations.filter((config) => config.id !== configId));
-        alert('Збірка видалена!');
-    } catch (error) {
-        console.error('Помилка видалення:', error);
-        alert('Не вдалося видалити збірку.');
-    }
+  try {
+    const configRef = doc(db, "user_configurations", user.uid, "configurations", configId);
+    await deleteDoc(configRef);
+    setConfigurations(configurations.filter(config => config.id !== configId)); 
+    alert("Збірка видалена!");
+  } catch (error) {
+    console.error("Помилка видалення:", error);
+    alert("Не вдалося видалити збірку.");
+  }
 };
